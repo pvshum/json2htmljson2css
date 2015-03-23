6e058dca11d2a9211d865c8967797545325b8bbc
Pavel <pv.shum@gmail.com>
2015-03-23 19:07:20 +1100

    Fixing closure bug

--- index.js
+++ index.js
@@ -14,31 +14,34 @@ module.exports = {
 		}
 		return outputCss;
 	},
-	makeHTML: function (rawObj, outputJHTML) {
-		var count = rawObj.length;
-		outputJHTML = outputJHTML || '';
-		for (var i = 0; i < count; i++) {
-			outputJHTML += '<' + rawObj[i].tag;
-			var attr = '';
-			for (var key in rawObj[i].attr) {
-				if (rawObj[i].attr.hasOwnProperty(key)) {
-					attr += ' ' + key + '="' + rawObj[i].attr[key] + '"';
+	makeHTML: function (rawObj) {
+		var make = function (rawObj, outputJHTML) {
+			var count = rawObj.length;
+			outputJHTML = outputJHTML || '';
+			for (var i = 0; i < count; i++) {
+				outputJHTML += '<' + rawObj[i].tag;
+				var attr = '';
+				for (var key in rawObj[i].attr) {
+					if (rawObj[i].attr.hasOwnProperty(key)) {
+						attr += ' ' + key + '="' + rawObj[i].attr[key] + '"';
+					}
+				}
+				attr += (rawObj[i].attr_str !== undefined) ? rawObj[i].attr_str : '';
+				outputJHTML += attr;
+				outputJHTML += (rawObj[i].content === undefined) ? '/>' : '>';
+				if (rawObj[i].content !== undefined) {
+					if (typeof rawObj[i].content === 'object') {
+						outputJHTML = make(rawObj[i].content, outputJHTML);
+					} else {
+						outputJHTML += rawObj[i].content;
+					}
+					outputJHTML += '</' + rawObj[i].tag + '>';
 				}
 			}
-			attr += (rawObj[i].attr_str !== undefined) ? rawObj[i].attr_str : '';
-			outputJHTML += attr;
-			outputJHTML += (rawObj[i].content === undefined) ? '/>' : '>';
-			if (rawObj[i].content !== undefined) {
-				if (typeof rawObj[i].content === 'object') {
-					outputJHTML = makeHTML(rawObj[i].content, outputJHTML);
-				} else {
-					outputJHTML += rawObj[i].content;
-				}
-				outputJHTML += '</' + rawObj[i].tag + '>';
+			if (i === count) {
+				return outputJHTML;
 			}
-		}
-		if (i === count) {
-			return outputJHTML;
-		}
+		};
+		return make(rawObj);
 	}
 };
