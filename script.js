582543b54bc9c29be18b31aa7c3f24451540f019
Pavel Shum <pv.shum@gmail.com>
2015-03-23 17:35:53 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var makeHTML = function(rawObj, outputJHTML) {
+    var count = rawObj.length;
+    outputJHTML = outputJHTML || '';
+    for (var i=0; i<count; i++) {
+        outputJHTML += '<' + rawObj[i].tag;
+        var attr = '';
+        for (var key in rawObj[i].attr) {
+            attr += ' ' + key + '="' + rawObj[i].attr[key] + '"';
+        }
+        attr += (rawObj[i].attr_str !== undefined) ? rawObj[i].attr_str : '';
+        outputJHTML += attr;
+        outputJHTML += (rawObj[i].content === undefined) ? '/>'  : '>';
+        if (rawObj[i].content !== undefined) {
+
+            if (typeof rawObj[i].content === 'object') {
+                makeHTML(rawObj[i].content, outputJHTML);
+            } else {
+                outputJHTML += rawObj[i].content;
+            }
+            outputJHTML += '</' + rawObj[i].tag + '>';
+        }
+    }
+    if (i == count ) {
+        return outputJHTML;
+    }
+};
